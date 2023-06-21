c30093988eca0687355f8502e6ed3acb8525309b
Rahul <gujjarrahul716@gmail.com>
2023-06-21 15:11:10 +0530

    hosting

--- src/settings.py
+++ src/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lbeb(nem!&m)*8_4ht3gcx#3yfpjk4s0698$)j=0y9=cvk#e_3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','TRahulT.pythonanywhere.com']
 
 
 # Application definition
